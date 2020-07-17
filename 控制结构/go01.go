a36361db0129476d213f355216214a0722fb88cc
国国国涛 <hello_lgt@163.com>
2020-07-17 18:28:39 +0800

    控制结构 ligt

---
+++ 控制结构/go01.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	bools := false
+	if bools {
+		fmt.Println("这是真")
+	} else {
+		fmt.Println("这是假")
+	}
+
+	var first int = 10
+	var cond int
+
+	if first <= 0 {
+		fmt.Printf("first is less than or equal to 0\n")
+	} else if first > 0 && first < 5 {
+		fmt.Printf("first is between 0 and 5\n")
+	} else {
+		fmt.Printf("first is 5 or greater\n")
+	}
+	if cond = 5; cond > 10 {
+		fmt.Printf("cond is greater than 10\n")
+	} else {
+		fmt.Printf("cond is not greater than 10\n")
+	}
+}
