87a80102f58ee9c42e29fb5cc4fcde0d6dd1d317
国国国涛 <hello_lgt@163.com>
2020-07-31 16:17:01 +0800

    基础

---
+++ 基础/go01.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("hello golang")
+	// fmt.Println("姓名李国涛\n")
+	// fmt.Println("籍贯河南\n")
+	// fmt.Println("地址：中国\n")
+
+	var a string = "a"
+	fmt.Println(a)
+
+}
