49651011d40d75989a32ad8715a0b6cd6e903518
国国国涛 <hello_lgt@163.com>
2020-08-03 10:44:37 +0800

    包大小写访问

---
+++ map/utils.go
@@ -0,0 +1,5 @@
+package map
+
+//定义一个变量
+
+var HeroName string = "宋江"
---
+++ 基础/go06.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"C:\Go_WorkSpace\golangLearn\map"
+)
+
+func main() {
+	fmt.Println(map.HeroName)
+}
