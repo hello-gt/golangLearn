4532f32d0238dfe5962cfdf36bf868bc3173f857
国国国涛 <hello_lgt@163.com>
2020-08-10 14:00:42 +0800

    测试包

---
+++ demo/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "Go_WorkSpace/golangLearn/demo/utils"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("测试包")
+	//utils.SayHello()
+}
---
+++ demo/utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"fmt"
+)
+
+func SayHello() {
+	fmt.PrintIn("Hello world")
+}
