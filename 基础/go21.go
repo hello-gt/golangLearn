4ce15f92eba1710ee846cfb7fe48a06c3958a728
国国国涛 <hello_lgt@163.com>
2020-08-13 10:28:11 +0800

    1

--- 基础/go21.go
+++ 基础/go21.go
@@ -3,13 +3,19 @@ package main
 import "fmt"
 
 func test(n1 int) {
-	n1 += 1
+	// n1 += 1
+
+	if n1 > 2 {
+		n1--
+		test(n1)
+	}
+
 	fmt.Println("test() n1= ", n1)
 
 }
 
 func main() {
-	n1 := 10
+	n1 := 4
 	test(n1)
-	fmt.Println("mian() n1=", n1)
+	// fmt.Println("mian() n1=", n1)
 }
