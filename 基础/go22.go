a9b9d073a39bbd7864b79456c3f4cdd99c4db86d
国国国涛 <hello_lgt@163.com>
2020-08-14 10:50:20 +0800

    细节02

--- 基础/go22.go
+++ 基础/go22.go
@@ -8,13 +8,16 @@ func myFun2(funvar myFunType, num1 int, num2 int) int {
 	return funvar(num1, num2)
 }
 
-func getSum(num1 int, num2 int) int {
-	return num1 + num2
+func getSum3(num1 int, num2 int) (res int, res2 int) {
+	res = num1 + num2
+	res2 = num1 - num2
+	return res, res2
 }
 
 func main() {
-	res := myFun2(getSum, 50, 60)
-	fmt.Println("res2=", res)
+	res, res2 := getSum3(50, 60)
+	fmt.Println("res=", res)
+	fmt.Println("res2=", res2)
 }
 
 //函数细节使用
